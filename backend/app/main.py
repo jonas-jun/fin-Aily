711f35ffde35672c51acbb278e9a00a02c10faaa
jonas-jun <junhot08@gmail.com>
2026-04-25 01:09:22 +0900

    add backend regex

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,6 +26,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins,
+    allow_origin_regex=r"https://fin-aily-.*\.vercel\.app", # Vercel 임시 주소 패턴 자동 허용
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
